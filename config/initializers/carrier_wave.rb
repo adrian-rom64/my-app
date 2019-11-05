e97692a5dd1fb4c97902a7a9479ee19ea6613adb
adrom99 <adrom99@gmail.com>
2019-11-05 10:14:03 +0100

    auto-commit

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,6 @@ if Rails.env.production?
       aws_access_key_id: 'AKIAURURPILK2TSYM6GB',
       aws_secret_access_key: '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'
     }
-    config.fog_directory = ENV['adrian-rails-app-2019-bucket']
+    config.fog_directory = 'adrian-rails-app-2019-bucket'
   end
 end
